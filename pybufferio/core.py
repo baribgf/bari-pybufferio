35c24accd88e8f15e17ed3df763774736a579584
bari <96777478+baribgf@users.noreply.github.com>
2024-08-14 11:18:09 +0000

    Some improvements and code refactoring

--- pybufferio/core.py
+++ pybufferio/core.py
@@ -1,6 +1,10 @@
 import shelve
+from os import remove
 from typing import NewType
 from collections.abc import Iterable
+
+from .utils import Direction
+from .utils import serialize_index
 
 class Buffer:
     """
@@ -10,7 +14,6 @@ class Buffer:
     __FromType = NewType('_from', int)
     __ToType = NewType('_to', int)
     __states = {'PENDING': '[*]', 'from-start': '[+]', 'from-end': '[-]', 'WARNING': '[!]', 'ERROR': '[!!]'}
-    directions = {'from-start': 1, 'from-end': -1}
 
     def __init__(self, buffer_path: str, verbose = True):
         """
@@ -28,10 +31,6 @@ class Buffer:
         if self.__verbose:
             print(*args)
 
-    def __serial_index(self, index):
-        """Serialize index with a serialized representation"""
-        return hex(index).replace('0x', '')
-
     def dump(self, __objects_array: Iterable):
         """
         Dump (write) the objects array into buffer.
@@ -46,7 +45,6 @@ class Buffer:
         if not isinstance(__objects_array, Iterable):
             raise TypeError(f'__objects_array must be an iterable instance, got: {__objects_array}')
             
-        from os import remove
         try:
             remove(self.__buffer_path + '.db')
             self.report(self.__states['WARNING'], f"File: '{self.__buffer_path}' replaced")
@@ -57,66 +55,44 @@ class Buffer:
             self.report(self.__states['PENDING'], f"Writing to: '{self.__buffer_path}'")
             index = 0
             for item in __objects_array:
-                db[self.__serial_index(index)] = item
+                db[serialize_index(index)] = item
                 index += 1
 
         self.report(self.__states['from-start'], f"File: {self.__buffer_path} written successfully")
         return self
 
-    def load(self, interval: tuple[__FromType, __ToType] = None, direction: int = directions['from-start']):
+    def load(self, interval: tuple[__FromType, __ToType] = (0, -1), direction: Direction = Direction.FROM_START):
         """
         Load the buffer items.
 
         Args:
             - `interval` -> (`tuple` [`__from`, `__to`]): The loading interval (`from` item index `to` another).
-            - `direction` -> (`int`): The loading direction ( `1` for `from-start`, `-1` for `from-end` ).
+            - `direction` -> (`Direction`): The loading direction ( FROM_START | FROM_END ).
         
         Returns:
             * A generator which yields all the buffered items.
         """
 
-        if not direction in self.directions.values():
-            raise ValueError(f'Invalid direction, got: {direction}\ndirection must be either 1 (Positive) -1 (Negative)')
+        if not (direction == Direction.FROM_START or direction == Direction.FROM_END):
+            raise ValueError(f'Invalid direction, got: {direction}')
 
-        if not isinstance(interval, tuple):
-            if interval is None and direction == self.directions['from-start']:
-                interval = (0, -1)
-
-            elif isinstance(interval, int):
-                if direction == self.directions['from-start']:
-                    interval = (0, interval)
-
-                else:
-                    interval = (interval, -1)
-            else:
-                raise ValueError(f'Invalid interval, got: {interval}')
-        elif len(interval) != 2:
-                raise ValueError(f'Invalid interval, got: {interval}')
+        if not isinstance(interval, tuple) or len(interval) < 2 or interval[1] < interval[0]:
+            raise ValueError(f'Invalid interval, got: {interval}')
 
         _from = interval[0]
         _to = interval[1]
-
-        if not (0 <= _from <= _to) and _to != -1:
-            raise ValueError(f"'_from', '_to' values are invalid, got {interval}")
 
         self.report(self.__states['PENDING'], f"Loading from: '{self.__buffer_path}' on: {interval}")
         try:
             with shelve.open(self.__buffer_path) as db:
                 if _to == -1:
-                    if direction == self.directions['from-start']:
-                        index = _from
-                    else:
-                        index = len(db) - _from
-                    item = db.get(self.__serial_index(index))
-                    while item is not None:
+                    index = _from if direction == Direction.FROM_START else len(db) - _from
+                    while (item := db.get(serialize_index(index))) is not None:
                         yield item
-                        item = db.get(self.__serial_index(index + 1))
                         index += 1
                 else:
-                    if direction == self.directions['from-start']:
-                        f, t = _from, _to
-                    else:
-                        f, t = len(db) - _from, len(db) - _to
+                    f, t = _from, _to if direction == Direction.FROM_START \
+                                    else len(db) - _from, len(db) - _to
 
                     for i in range(f, t):
                         yield db.get(str(i))
@@ -125,17 +101,17 @@ class Buffer:
         except FileNotFoundError:
             self.report(self.__states['ERROR'], f"File: '{self.__buffer_path}' not found!")
 
-    # Not really useful !!
-    def __load_raw(self, interval: tuple[__FromType, __ToType] = None, direction: int = directions['from-start']):
-        if not direction in self.directions.values():
-            raise ValueError(f'Invalid direction, got: {direction}\ndirection must be either 1 (Positive) -1 (Negative)')
+    # FIXME: Not really useful !!
+    def __load_raw(self, interval: tuple[__FromType, __ToType] = (0, -1), direction: Direction = Direction.FROM_START):
+        if not (direction == Direction.FROM_START or direction == Direction.FROM_END):
+            raise ValueError(f'Invalid direction, got: {direction}')
 
         if not isinstance(interval, tuple):
-            if interval is None and direction == self.directions['from-start']:
+            if interval is None and direction == Direction.FROM_START:
                 interval = (0, -1)
 
             elif isinstance(interval, int):
-                if direction == self.directions['from-start']:
+                if direction == Direction.FROM_START:
                     interval = (0, interval)
 
                 else:
@@ -154,7 +130,7 @@ class Buffer:
         self.report(self.__states['PENDING'], f"Loading from: '{self.__buffer_path}' on: {interval}")
         try:
             with open(self.__buffer_path + '.db', 'rb') as f:
-                if direction == self.directions['from-start']:
+                if direction == Direction.FROM_START:
                     f.seek(_from)
                 else:
                     f.seek(0, 2)
@@ -169,7 +145,7 @@ class Buffer:
                         yield char
 
                 else:
-                    if direction == self.directions['from-start']:
+                    if direction == Direction.FROM_START:
                         while True:
                             if f.tell() == _to:
                                 break
@@ -201,7 +177,7 @@ class Buffer:
             * A generator which yields first `n` buffered items.
         """
 
-        return self.load((0, n), self.directions['from-start'])
+        return self.load((0, n), Direction.FROM_START)
 
     def tail(self, n: int = 10):
         """
@@ -214,9 +190,4 @@ class Buffer:
             * A generator which yields last `n` buffered items.
         """
 
-        return self.load((n, -1), self.directions['from-end'])
-
-"""
-Todo:
-    - Progress
-"""
+        return self.load((n, -1), Direction.FROM_END)
